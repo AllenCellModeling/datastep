e57cf64b73fd7fcf63e8d41f44340874f8fb2f61
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-01-13 16:21:14 -0800

    Add constants file with defaults

---
+++ step/constants.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+
+
+CONFIG_ENV_VAR_NAME = "STEP_CONFIG"
+CWD_CONFIG_FILE_NAME = "step_config.json"
+
+DEFAULT_QUILT_STORAGE = "s3://allencell-internal-quilt"
+DEFAULT_LOCAL_TEMP_OUTPUTS = "{cwd}/temp/{module_name}"
