7fbf0015cd056fccd34c82d9126182964936d372
Jackson Brown <jmaxfieldbrown@gmail.com>
2020-01-14 16:17:58 -0800

    Add tests for file_utils

---
+++ step/tests/test_file_utils.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+from step import file_utils
+
+###############################################################################
+
+
+@pytest.mark.parametrize(
+    "f",
+    [
+        ("example_config_1.json"),
+        pytest.param(
+            "example_directory", marks=pytest.mark.raises(exceptions=IsADirectoryError)
+        ),
+        pytest.param(
+            "not_a_file.json", marks=pytest.mark.raises(exceptions=FileNotFoundError)
+        ),
+    ],
+)
+def test_resolve_filepath(data_dir, f):
+    # Route to data dir
+    f = data_dir / f
+
+    # Run
+    file_utils.resolve_filepath(f)
+
+
+@pytest.mark.parametrize(
+    "f",
+    [
+        ("example_directory"),
+        pytest.param(
+            "example_config_1.json",
+            marks=pytest.mark.raises(exceptions=FileExistsError),
+        ),
+    ],
+)
+def test_resolve_directory(data_dir, f):
+    # Route to data dir
+    f = data_dir / f
+
+    # Run
+    file_utils.resolve_directory(f)
