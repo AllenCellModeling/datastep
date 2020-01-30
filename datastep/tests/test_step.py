e56c2fa60bcaa8fe85ee27eaadf38bcdf3649694
Rory Donovan-Maiye <donovanr@alumni.reed.edu>
2020-01-30 14:08:55 -0800

    update Test class to make and log empty data files

--- datastep/tests/test_step.py
+++ datastep/tests/test_step.py
@@ -6,6 +6,7 @@ import shutil
 from pathlib import Path
 
 import pytest
+import pandas as pd
 
 from datastep import Step, constants, file_utils
 
@@ -17,8 +18,23 @@ class Test(Step):
     def __init__(self, direct_upstream_tasks=None, config=None):
         super().__init__(direct_upstream_tasks, config)
 
-    def run(self, a=1):
-        print(a)
+    def run(self, N=3):
+        # make a directory of empty files
+        imdir = self.step_local_staging_dir / Path("files")
+        imdir.mkdir(parents=True, exist_ok=True)
+
+        # make a dataframe logging their absolute paths
+        self.manifest = pd.DataFrame(index=range(N), columns=["filepath"])
+        for i in range(N):
+            path = imdir / Path(f"file{i}.txt")
+            path = path.resolve()
+            path.touch()
+            self.manifest.at[i, "filepath"] = path
+
+        # save manifest
+        self.manifest.to_csv(
+            self.step_local_staging_dir / Path("manifest.csv"), index=False
+        )
 
 
 ###############################################################################
