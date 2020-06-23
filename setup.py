516383251a138bdbe7aadbfe0218231378f7ce23
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-06-22 17:31:15 -0700

    Upgrade package from cookiecutter

--- setup.py
+++ setup.py
@@ -8,35 +8,34 @@ from setuptools import find_packages, setup
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-test_requirements = [
-    "codecov",
-    "flake8",
-    "black",
-    "pytest",
-    "pytest-cov",
-    "pytest-raises",
+setup_requirements = [
+    "pytest-runner>=5.2",
 ]
 
-setup_requirements = ["pytest-runner"]
+test_requirements = [
+    "black>=19.10b0",
+    "codecov>=2.1.4",
+    "flake8>=3.8.3",
+    "flake8-debugger>=3.2.1",
+    "pytest>=5.4.3",
+    "pytest-cov>=2.9.0",
+    "pytest-raises>=0.11",
+]
 
 dev_requirements = [
-    "bumpversion>=0.5.3",
-    "coverage>=5.0a4",
-    "flake8>=3.7.7",
-    "ipython>=7.5.0",
+    *setup_requirements,
+    *test_requirements,
+    "bumpversion>=0.6.0",
+    "coverage>=5.1",
+    "ipython>=7.15.0",
     "m2r>=0.2.1",
-    "pytest>=4.3.0",
-    "pytest-cov==2.6.1",
-    "pytest-raises>=0.10",
-    "pytest-runner>=4.4",
-    "Sphinx>=2.0.0b1",
-    "sphinx_rtd_theme>=0.1.6",
-    "tox>=3.5.2",
-    "twine>=1.13.0",
-    "wheel>=0.33.1",
+    "pytest-runner>=5.2",
+    "Sphinx>=2.0.0b1,<3",
+    "sphinx_rtd_theme>=0.4.3",
+    "tox>=3.15.2",
+    "twine>=3.1.1",
+    "wheel>=0.34.2",
 ]
-
-interactive_requirements = ["altair", "jupyterlab", "matplotlib"]
 
 requirements = [
     "boto3>=1.11.9",
@@ -53,17 +52,13 @@ requirements = [
 ]
 
 extra_requirements = {
-    "test": test_requirements,
     "setup": setup_requirements,
+    "test": test_requirements,
     "dev": dev_requirements,
-    "interactive": interactive_requirements,
     "all": [
         *requirements,
-        *test_requirements,
-        *setup_requirements,
         *dev_requirements,
-        *interactive_requirements,
-    ],
+    ]
 }
 
 setup(
@@ -74,18 +69,21 @@ setup(
         "Intended Audience :: Developers",
         "License :: Free for non-commercial use",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.6"
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    description="A base class and utilities for creating steps in DAGs that are tied "
-    "to large amounts of data.",
+    description=(
+        "A base class and utilities for creating steps in DAGs that are tied "
+        "to large amounts of data."
+    ),
     entry_points={"console_scripts": ["make_new_step=datastep.bin.make_new_step:main"]},
     install_requires=requirements,
     license="Allen Institute Software License",
     long_description=readme,
     long_description_content_type="text/markdown",
     include_package_data=True,
-    keywords="datastep, DAG, data, stepwise",
+    keywords="datastep, DAG, pipeline, workflow",
     name="datastep",
     packages=find_packages(exclude=["tests", "*.tests", "*.tests.*"]),
     python_requires=">=3.6",
