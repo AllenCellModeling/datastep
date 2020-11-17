cd6ea4a17a5bec441dceb221660e4599404ac020
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2020-11-16 17:20:33 -0800

    Bump version: 0.1.8 → 0.1.9

--- setup.py
+++ setup.py
@@ -97,6 +97,6 @@ setup(
     url="https://github.com/AllenCellModeling/datastep",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.1.8",
+    version="0.1.9",
     zip_safe=False,
 )
